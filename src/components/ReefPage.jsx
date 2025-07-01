7db98b9ae2708f386259c15bce2d2d7e9c699cd8
aldojaimejuarez <aldojaimejuarez@gmail.com>
2025-07-01 23:56:16 +1000

    new reef link video

--- src/components/ReefPage.jsx
+++ src/components/ReefPage.jsx
@@ -447,7 +447,7 @@ function ReefPage() {
             id="reefVideo"
             width="100%"
             height="220"
-            src="https://www.youtube.com/embed/FphhpDrweVI"
+            src="https://www.youtube.com/embed/h8YvxzlrobY"
             title="Reef Analytics Demo Video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
